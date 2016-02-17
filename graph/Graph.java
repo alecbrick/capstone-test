5b50bcb2a96d57bd115792de1fbdffaf4d7ac216
Alec Brickner <alecjbrick@hotmail.com>
2016-02-16 19:15:37 -0800

    Create graph printing function

--- graph/Graph.java
+++ graph/Graph.java
@@ -15,4 +15,7 @@ public interface Graph {
     /* Returns all SCCs in a directed graph. Don't worry about handling
      * this for undirected graphs. */
     public List<Graph> getSCCs();
+    
+    /* Return the graph's connections in a printable format. */
+    public String printGraph();
 } 
